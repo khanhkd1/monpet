c015a4a2d877b33466688accd1cd3153e9e555ce
Kiều Duy Khánh <khanhkd1@khanhkd1.local>
2022-06-03 23:30:15 +0700

    add model database

--- store/models.py
+++ store/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, null=False)
+
+    class Meta:
+        db_table = 'category'
+
+
+class Product(models.Model):
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, null=False)
+    price = models.IntegerField()
+    avaiable = models.BooleanField(default=True)
+    describe = models.TextField()
+
+    class Meta:
+        db_table = 'product'
+
+
+class Image(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image_url = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return self.image_url
+
+    class Meta:
+        db_table = 'image'

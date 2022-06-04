29cb1befdcd9fb34d15f425eabcdcbf3b5adb167
Kiều Duy Khánh <khanhkd1@khanhkd1.local>
2022-06-04 18:02:54 +0700

    remove admin site default. add new admin site

--- store/models.py
+++ store/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=255, null=False)
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         db_table = 'category'
 
@@ -14,6 +17,9 @@ class Product(models.Model):
     price = models.IntegerField()
     avaiable = models.BooleanField(default=True)
     describe = models.TextField()
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         db_table = 'product'
--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = "store"
+
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path("admin/login/", views.AdminLoginView.as_view(), name="adminlogin"),
+    path("admin/logout/", views.AdminLogoutView.as_view(), name="adminlogout"),
+    path("admin/", views.AdminHomeView.as_view(), name="adminhome"),
 ]
